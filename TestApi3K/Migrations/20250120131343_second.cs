feeeafd21af53cae0218c5ff7bfc6b91109f3393
MorFyNC <bulat.hisambiev@gmail.com>
2025-01-20 20:50:54 +0300

    Добавьте файлы проекта.

---
+++ TestApi3K/Migrations/20250120131343_second.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestApi3K.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descrioption",
+                table: "Users",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role_id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Role_id",
+                table: "Users",
+                column: "Role_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_Role_id",
+                table: "Users",
+                column: "Role_id",
+                principalTable: "Roles",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_Role_id",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Role_id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role_id",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Users",
+                newName: "Descrioption");
+        }
+    }
+}
---
+++ TestApi3K/Service/UserLoginService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestApi3K.DataBaseContext;
+using TestApi3K.Interfaces;
+using TestApi3K.Model;
+using TestApi3K.Requests;
+
+namespace TestApi3K.Service
+{
+    public class UserLoginService : IUsersLoginsService
+    {
+        private readonly ContextDb _context;
+
+        public UserLoginService(ContextDb context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> GetAllUsersAsync()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return new OkObjectResult(new
+            {
+                data = new { users = users },
+                status = true
+            });
+        }
+
+        public async Task<IActionResult> CreateNewUserAndLoginAsync(CreateNewUserAndLogin newUser)
+        {
+            var user = new Users()
+            {
+                Name = newUser.Name,
+                Description = newUser.Description,
+            };
+
+            var login = new Logins()
+            {
+                User_id = user.id_User,
+                Login = newUser.Login,
+                Password = newUser.Password,
+            };
+
+            var existinglogins = await _context.Logins.ToListAsync();
+            if (existinglogins.Any(x => x.Login.ToLower() == login.Login.ToLower()))
+            {
+                return new ConflictObjectResult(new { status = false, message = "Пользователь с таким логином уже существует" });
+            }
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            await _context.Logins.AddAsync(login);
+            await _context.SaveChangesAsync();
+
+            return new OkObjectResult(new
+            {
+                status = true,
+                message = "none"
+            });
+        }
+    }
+}
